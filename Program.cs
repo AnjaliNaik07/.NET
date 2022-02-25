0750bd7f595c7b6ec4a3079f4eb29ed3e026e137
AnjaliNaik07 <99865210+AnjaliNaik07@users.noreply.github.com>
2022-02-25 11:02:18 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
 ﻿using System;
-public class FibonacciExample
+public class SumExample
 {
     public static void Main(string[] args)
     {
-        int n1 = 0, n2 = 1, n3, i, number;
-        Console.Write("Enter the number of elements: ");
-        number = int.Parse(Console.ReadLine());
-        Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
-        for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+        int n, sum = 0, m;
+        Console.Write("Enter a number: ");
+        n = int.Parse(Console.ReadLine());
+        while (n > 0)
         {
-            n3 = n1 + n2;
-            Console.Write(n3 + " ");
-            n1 = n2;
-            n2 = n3;
+            m = n % 10;
+            sum = sum + m;
+            n = n / 10;
         }
+        Console.Write("Sum is= " + sum);
     }
 }
