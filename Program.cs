4d6210b4462c1457b6dd0398018b4087a057c76e
AnjaliNaik07 <99865210+AnjaliNaik07@users.noreply.github.com>
2022-02-25 12:07:02 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,40 @@
 ﻿using System;
-public class PrimeNumberExample
+namespace Exercises
 {
-    public static void Main(string[] args)
+    class Box
     {
-        int n, i, m = 0, flag = 0;
-        Console.Write("Enter the Number to check Prime: ");
-        n = int.Parse(Console.ReadLine());
-        m = n / 2;
-        for (i = 2; i <= m; i++)
+        float width;
+        float height;
+        float length;
+        public float Volume
         {
-            if (n % i == 0)
-            {
-                Console.Write("Number is not Prime.");
-                flag = 1;
-                break;
-            }
+            get { return width * height * length; }
         }
-        if (flag == 0)
-            Console.Write("Number is Prime.");
+        public Box(float width, float height, float length)
+        {
+            this.width = width;
+            this.height = height;
+            this.length = height;
+        }
+        public static float operator +(Box box1, Box box2)
+        {
+            return box1.Volume + box2.Volume;
+        }
+        public override String ToString()
+        {
+            return "box with width " + width + ", height " + height + " and length " + length;
+        }
+    }
+    class OperatorOverloading
+    {
+        public static void Main()
+        {
+            Box box1 = new Box(10, 20, 30);
+            Box box2 = new Box(25, 32, 15);
+            Console.WriteLine("Volume of {0} is: {1}", box1, box1.Volume);
+            Console.WriteLine("Volume of {0} is: {1}", box2, box2.Volume);
+
+        Console.WriteLine("Volume after adding boxes: {0}", box1 + box2);
+        }
     }
 }
