666819f9b259b1deba66b7304822b068c2d94a01
AnjaliNaik07 <99865210+AnjaliNaik07@users.noreply.github.com>
2022-02-25 10:58:44 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-public class ArmstrongExample
+public class ReverseExample
 {
     public static void Main(string[] args)
     {
-        int n, r, sum = 0, temp;
-        Console.Write("Enter the Number= ");
+        int n, reverse = 0, rem;
+        Console.Write("Enter a number: ");
         n = int.Parse(Console.ReadLine());
-        temp = n;
-        while (n > 0)
+        while (n != 0)
         {
-            r = n % 10;
-            sum = sum + (r * r * r);
-            n = n / 10;
+            rem = n % 10;
+            reverse = reverse * 10 + rem;
+            n /= 10;
         }
-        if (temp == sum)
-            Console.Write("Armstrong Number.");
-        else
-            Console.Write("Not Armstrong Number.");
+        Console.Write("Reversed Number: " + reverse);
     }
 }
